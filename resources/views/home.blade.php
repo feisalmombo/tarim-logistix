65dddc37408d3b6f993924f8238ac3acc2882ffa
feisalmombo <feisalmombo29@gmail.com>
2023-06-13 12:58:37 +0300

    WIP

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,22 +39,22 @@
 <section class="content">
     <div class="row">
 
-        @if(Auth::user()->hasRole('developer') || Auth::user()->hasRole('staff'))
-        <div class="col-lg-4">
-            <a href="#">
-            <div class="info-box">
-            <!-- Apply any bg-* class to to the icon to color it -->
-            <span class="info-box-icon bg-light-blue"><i class="fa fa-users"></i></span>
-            <div class="info-box-content">
-              <span class="info-box-text">Users</span>
-              <span class="info-box-number">{{ $usersCount[0]->usersCount }}</span>
-            </div><!-- /.info-box-content -->
-          </div><!-- /.info-box -->
-          </a>
-        </div>
-     @endif
 
-        @if(Auth::user()->hasRole('developer') || Auth::user()->hasRole('staff'))
+            <div class="col-lg-4">
+                <a href="#">
+                <div class="info-box">
+                <!-- Apply any bg-* class to to the icon to color it -->
+                <span class="info-box-icon bg-light-blue"><i class="fa fa-users"></i></span>
+                <div class="info-box-content">
+                <span class="info-box-text">Users</span>
+                <span class="info-box-number">34</span>
+                </div><!-- /.info-box-content -->
+            </div><!-- /.info-box -->
+            </a>
+            </div>
+
+
+
         <div class="col-lg-4">
             <a href="#">
             <div class="info-box">
@@ -62,7 +62,7 @@
             <span class="info-box-icon bg-light-blue"><i class="fa fa-square"></i></span>
             <div class="info-box-content">
               <span class="info-box-text">Posts</span>
-              <span class="info-box-number">{{ $postsCount[0]->postsCount }}</span>
+              <span class="info-box-number">56</span>
             </div><!-- /.info-box-content -->
           </div><!-- /.info-box -->
          </a>
@@ -75,15 +75,15 @@
             <span class="info-box-icon bg-blue"><i class="fa fa-indent"></i></span>
             <div class="info-box-content">
               <span class="info-box-text">Categories</span>
-              <span class="info-box-number">{{ $categoriesCount[0]->categoriesCount }}</span>
+              <span class="info-box-number">89</span>
             </div><!-- /.info-box-content -->
           </div><!-- /.info-box -->
            </a>
         </div>
-     @endif
 
 
-     @if(Auth::user()->hasRole('developer') || Auth::user()->hasRole('staff'))
+
+
      <div class="col-lg-4">
          <a href="#">
          <div class="info-box">
@@ -91,7 +91,7 @@
          <span class="info-box-icon bg-light-blue"><i class="fa fa-user-plus"></i></span>
          <div class="info-box-content">
            <span class="info-box-text">Subscribers</span>
-           <span class="info-box-number">{{ $subscribersCount[0]->subscribersCount }}</span>
+           <span class="info-box-number">90</span>
          </div><!-- /.info-box-content -->
        </div><!-- /.info-box -->
       </a>
@@ -104,26 +104,11 @@
          <span class="info-box-icon bg-blue"><i class="fa fa-comment"></i></span>
          <div class="info-box-content">
            <span class="info-box-text">Comments</span>
-           <span class="info-box-number">{{ $commentsCount[0]->commentsCount }}</span>
+           <span class="info-box-number">656</span>
          </div><!-- /.info-box-content -->
        </div><!-- /.info-box -->
         </a>
      </div>
-
-
-    {{--<div class="col-lg-4">
-    <a href="{{ url('/request-customer') }}">
-            <div class="info-box">
-            <!-- Apply any bg-* class to to the icon to color it -->
-            <span class="info-box-icon bg-blue"><i class="fa fa-registered"></i></span>
-            <div class="info-box-content">
-              <span class="info-box-text">Request</span>
-              <span class="info-box-number">9</span>
-            </div><!-- /.info-box-content -->
-          </div><!-- /.info-box -->
-           </a>
-    </div> --}}
-    @endif
 
 </section>
 
